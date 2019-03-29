9b481fb21f9170ba1f38c86d478de0ce9e1a99e5
aureliedouet <aurelie.douet@parisgeo.cnrs.fr>
2019-03-29 11:08:13 +0100

    create function p2m
    Former-commit-id: d20c1a58d76386487f6aaa91a63fefe8585afeee

--- mobiliscope_v3/scriptsr/4_p2m_fct.R
+++ mobiliscope_v3/scriptsr/4_p2m_fct.R
@@ -8,8 +8,10 @@
 
 
 
-# 1. TRANSFO DE LA TABLE DES PRESENCES (autonomes et "immobiles" en un lieu) AU FORMAT LARGE
 
+################## GLOBAL FONCTIONS #########################
+
+# 1. TRANSFO DE LA TABLE DES PRESENCES (autonomes et "immobiles" en un lieu) AU FORMAT LARGE
 prepPrezWide <- function(data){
   
   # Sélection des mobilités autonomes
@@ -43,10 +45,7 @@ prepPrezWide <- function(data){
 }
 
 
-
-
 # 2. PREPARATION DE L'INDICATEUR "WHOLE POPULATION"
-
 createPopFiles <- function(nomEnq){
   
   # 2.a. CONSTRUCTION DES DONNEES POUR LES OURSINS - pop0_flow : 
@@ -246,12 +245,7 @@ createPopFiles <- function(nomEnq){
 }
 
 
-
-
-
 # 3. CREATION DES TABLES POUR LA FONCTION "createFiles"
-#============================================================================#
-
 
 ## 3a. INDICATEUR : SEX
 
@@ -963,12 +957,8 @@ createFiles <- function(nbMod, indicateur, nomIndic, nomEnq, pvs, pvs2, pvs3){
   
 }  
   
-############################################################################################
 
-
-
-
-################## FONCTION : création des indices de ségrégation #########################  
+# 5.FONCTION : création des indices de ségrégation 
 
 createISeg <- function(nbMod, nomIndic, nomEnq, pvs, pvs2){
   
@@ -1074,9 +1064,7 @@ createISeg <- function(nbMod, nomIndic, nomEnq, pvs, pvs2){
 }  
 
 
-
-
-################## FONCTION : création des graphiques en barres empilées (stacked) #########################  
+# 6.FONCTION : création des graphiques en barres empilées (stacked) 
 
 createStacked <- function(nbMod, nomIndic, nomEnq){
   
@@ -1266,4 +1254,225 @@ createStacked <- function(nbMod, nomIndic, nomEnq){
 
 }  
 
-############################################################################################
+
+
+
+
+######################### ALGO FONCTION ######################### 
+
+## Fonction p2m : de la table de présence aux indicateurs du Mobiliscope
+
+p2m <- function(nomEnq){
+  
+  # Création des répertoires "stacked" et "indice_segreg"
+  dir.create(paste("www/data/",nomEnq, sep = ''))
+  dir.create(paste("www/data/",nomEnq,"/stacked", sep = ''))
+  dir.create(paste("www/data/",nomEnq,"/indice_segreg", sep = ''))
+  
+  # 2. INDICATEUR "WHOLE POPULATION" (et dossier "INDIC")
+  createPopFiles(nomEnq)
+  
+  # 3. Préparation des données par indicateur 
+  
+  ## 3a. SEX 
+  nbMod <- length(unique(prez_wide_h$SEX))
+  nomIndic <- "sex"
+  indicateur <- prez_wide_h$SEX
+  
+  prezStock <- prepStock_sex(nomEnq)
+  prezPart <- prepPart_sex(pvs = prezStock)
+  prezNR <- prepNR_sex()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3b. AGE
+  nbMod <- length(unique(prez_wide_h$KAGE))
+  nomIndic <- "age"
+  indicateur <- prez_wide_h$KAGE
+  
+  prezStock <- prepStock_age(nomEnq)
+  prezPart <- prepPart_age(pvs = prezStock)
+  prezNR <- prepNR_age()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3c. NiVEAU D'EDUCATION (INDIVIDUEL)
+  nbMod <- length(unique(prez_wide_h$EDUC))-1
+  nomIndic <- "cleduc"
+  indicateur <- prez_wide_h$EDUC
+  
+  prezStock <- prepStock_educ(nomEnq)
+  prezPart <- prepPart_educ(pvs = prezStock)
+  prezNR <- prepNR_educ()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3d. NiVEAU D'EDUCATION (MENAGE) 
+  nbMod <- length(unique(prez_wide_h$EDUCMEN))-1
+  nomIndic <- "educmen"
+  indicateur <- prez_wide_h$EDUCMEN
+  
+  prezStock <- prepStock_educMen(nomEnq)
+  prezPart <- prepPart_educMen(pvs = prezStock)
+  prezNR <- prepNR_educMen()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  
+  ## 3f. CSP (INDIVIDUELLE) 
+  nbMod <- length(unique(prez_wide_h$CSP))-1
+  nomIndic <- "cs"
+  indicateur <- prez_wide_h$CSP
+  
+  prezStock <- prepStock_csp(nomEnq)
+  prezPart <- prepPart_csp(pvs = prezStock)
+  prezNR <- prepNR_csp()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3g. CSP (MENAGE) 
+  nbMod <- length(unique(prez_wide_h$CSPMEN))-1
+  nomIndic <- "cspmen"
+  indicateur = prez_wide_h$CSPMEN
+  
+  prezStock <- prepStock_cspMen(nomEnq)
+  prezPart <- prepPart_cspMen(pvs = prezStock)
+  prezNR <- prepNR_cspMen()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3h. STATUT D'OCCUPATION 
+  nbMod <- length(unique(prez_wide_h$OCC))-1
+  nomIndic <- "occ"
+  indicateur = prez_wide_h$OCC
+  
+  prezStock <- prepStock_occ(nomEnq)
+  prezPart <- prepPart_occ(pvs = prezStock)
+  prezNR <- prepNR_occ()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  
+  ## 3j. ZONE DE RESIDENCE (RESIDENTIAL RINGS)
+  nbMod <- length(unique(prez_wide_h$ZONAGE))
+  nomIndic <- "resarea"
+  indicateur <- prez_wide_h$ZONAGE
+  
+  prezStock <- prepStock_zone(nomEnq)
+  prezPart <- prepPart_zone(pvs = prezStock)
+  prezNR <- prepNR_zone()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  
+  ## 3k. ACTIVITE (attention -2 pour Paris)
+  nbMod <- length(unique(prez_wide_h$MOTIF))-1
+  nomIndic <- "act"
+  indicateur <- prez_wide_h$MOTIF
+  
+  prezStock <- prepStock_act(nomEnq)
+  prezPart <- prepPart_act(pvs = prezStock)
+  prezNR <- prepNR_act()
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+  ## 3l. MODE DE TRANSPORT 
+  nbMod <- length(unique(prez_wide_h$MODE_ARR))-1
+  nomIndic <- "mode"
+  indicateur <- prez_wide_h$MODE_ARR
+  
+  prezStock <- prepStock_mode(nomEnq)
+  prezPart <- prepPart_mode(pvs = prezStock)
+  prezNR <- prepNR_mode()
+  
+  
+  # 4. CREATION DES FICHIERS POUR LA CARTOGRAPHIE ET LE GRAPHIQUE "SIMPLE" :
+  createFiles(nbMod, indicateur, nomIndic, nomEnq, 
+              pvs = prezStock, pvs2 = prezPart, pvs3 = prezNR)
+  
+  # 5. CREATION DES FICHIERS POUR LE GRAPHIQUE SEGREGATION (INDICES DE DUNCAN ET DE MORAN)
+  createISeg(nbMod, nomIndic, nomEnq, pvs = prezStock, pvs2 = prezPart)
+  
+  # 6. CREATION DES FICHIERS POUR LE GRAPHIQUE "STACKED"
+  createStacked(nbMod, nomIndic, nomEnq)
+  
+}
